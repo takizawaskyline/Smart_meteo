5728fef881df91f068a7a155ab714f9d7f8d370d
takizawaskyline <vorfolomeevnekoglaj@gmail.com>
2026-05-02 21:06:07 +0300

    Update main.ino
    mq-2 added

--- Arduino/main.ino
+++ Arduino/main.ino
@@ -1,26 +1,42 @@
 #include <Wire.h> 
+#include <TroykaMQ.h>
 #include <LiquidCrystal_I2C.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <GyverBME280.h>
 #include <ArduinoJson.h>
-LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 GyverBME280 bme;
+
 
 const char* ssid = "Keenetic-3618";
 const char* password = "bzwCZBUk";
 
-const char* mqtt_server = "srv2.clusterfly.ru";
-const int mqtt_port = 9991;
-const char* mqtt_user = "user_159400dd";
-const char* mqtt_pass = "IsAXcJDu4RQA4";
+const char* mqtt_server = "m4.wqtt.ru";
+const int mqtt_port = 15934;
+const char* mqtt_user = "u_1R9J5Q";
+const char* mqtt_pass = "aC0jfFJi";
 const char* mqtt_topic_sens_pub = "user_159400dd/sens_val";
 
+int hydrogen = 0;
+int smoke = 0;
+int methane = 0;
+int LPG = 0;
+int ratio = 0;
+
+
 WiFiClient espClient;
+
 PubSubClient client(espClient);
 bool mqttConnected = false;  // Флаг подключения
 
+#define PIN_MQ2 34
+#define PIN_MQ2_HEATER  26
+MQ2 mq2(PIN_MQ2, PIN_MQ2_HEATER);
+
 void setup() {
+  mq2.heaterPwrHigh();
+  
   bme.begin(0x77);
   if (!bme.begin(0x77)) Serial.println("Error!");
   client.setServer(mqtt_server, mqtt_port);
@@ -35,7 +51,21 @@ void setup() {
 }
 
 void loop() {
-
+     if (!mq2.isCalibrated() && mq2.heatingCompleted()) {
+    // выполняем калибровку датчика на чистом воздухе
+    mq2.calibrate();
+    // выводим сопротивление датчика в чистом воздухе (Ro) в serial-порт
+    Serial.print("Ro = ");
+    Serial.println(mq2.getRo());
+  }
+     if (mq2.isCalibrated() && mq2.heatingCompleted()) {
+    hydrogen = mq2.readHydrogen();
+    smoke = mq2.readSmoke();
+    methane = mq2.readMethane();
+    LPG = mq2.readLPG();
+    ratio = mq2.readRatio();
+  }
+  mqtt_connect(); 
   client.loop();
   //Публикация сообещния
   char msg[10];
@@ -43,6 +73,7 @@ void loop() {
   String data_json = sens_val();
   client.publish(mqtt_topic_sens_pub, data_json.c_str());
   Serial.println(data_json);
+  otladka();
   delay(1000);
 
 }
@@ -73,6 +104,29 @@ void otladka() {
   Serial.print("Pressure: ");
   Serial.println(bme.readPressure() / 133,32);
 
+  Serial.print("Ro = ");
+  Serial.println(mq2.getRo());
+
+  if (mq2.isCalibrated() && mq2.heatingCompleted()) {
+    // выводим отношения текущего сопротивление датчика
+    // к сопротивлению датчика в чистом воздухе (Rs/Ro)
+    Serial.print("Ratio: ");
+    Serial.print(mq2.readRatio());
+    // выводим значения газов в ppm
+    Serial.print("LPG: ");
+    Serial.print(mq2.readLPG());
+    Serial.print(" ppm ");
+    Serial.print(" Methane: ");
+    Serial.print(mq2.readMethane());
+    Serial.print(" ppm ");
+    Serial.print(" Smoke: ");
+    Serial.print(mq2.readSmoke());
+    Serial.print(" ppm ");
+    Serial.print(" Hydrogen: ");
+    Serial.print(mq2.readHydrogen());
+    Serial.println(" ppm ");
+    //delay(100);
+  }
 }
 
 void wifi() {
@@ -119,22 +173,31 @@ void reconnectMQTT() {
     Serial.println(client.state());
   }
 }
+
+
 String sens_val() {
   int val_temp = bme.readTemperature();
   int val_h = bme.readHumidity();
   int val_pressure = bme.readPressure() / 133.3;
-  String json = json_file(&val_h, &val_temp, &val_pressure);
+
+
+  String json = json_file(&val_h, &val_temp, &val_pressure, &hydrogen, &smoke, &methane, &LPG, &ratio);
 
   return json;
 }
 
 
-String json_file(int* val_h, int* val_temp, int*val_pressure) {
+String json_file(int* val_h, int* val_temp, int* val_pressure, int* hydrogen, int* smoke, int* methane, int* LPG, int* ratio) {
   String jsonchik = "{";
   jsonchik += "\"sens_val\": {";
   jsonchik += "\"hum\":" + String(*val_h) + ",";
   jsonchik += "\"temp\":" + String(*val_temp) + ",";
-  jsonchik += "\"pressure\":" + String(*val_pressure);
+  jsonchik += "\"pressure\":" + String(*val_pressure) + ",";  // ← Добавлена запятая
+  jsonchik += "\"hydrogen\":" + String(*hydrogen) + ",";
+  jsonchik += "\"smoke\":" + String(*smoke) + ",";
+  jsonchik += "\"methane\":" + String(*methane) + ",";
+  jsonchik += "\"LPG\":" + String(*LPG) + ",";
+  jsonchik += "\"ratio\":" + String(*ratio);
   jsonchik += "}}";
 
   return jsonchik;
